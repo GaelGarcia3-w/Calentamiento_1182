d09c425fea7d871c4cdd583778d2847ce9ccc3d6
Galletas <a.23308051281182@cbtis128.edu.mx>
2024-11-22 23:25:16 -0600

    Create 4.py

---
+++ 4.py
@@ -0,0 +1,13 @@
+print()
+print("Edgar gael Garcia Camaco 1182 3w:Calentamiento")
+print()
+g = input("Ingresa una letra: ").lower()  # Convierte la letra a minúscula
+def vocal():
+    if g == "a" or g == "e" or g == "i" or g == "o" or g == "u":
+        print()
+        print("Verdad")
+    else:
+        print()
+        print("Mentira")
+
+vocal()
