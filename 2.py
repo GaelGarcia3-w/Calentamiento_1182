fc900c0a20fb48e21495331962df7ab18e155f95
Galletas <a.23308051281182@cbtis128.edu.mx>
2024-11-22 23:24:34 -0600

    Create 2.py

---
+++ 2.py
@@ -0,0 +1,13 @@
+print()
+print("Edgar Gael Garcia Camacho 1182 3w:Calentamiento")
+print()
+def max_3(a,b,c):
+    if a > b and c:
+        print(a)
+    elif b > c and a:
+        print(b)
+    elif c > a and b:
+       print(c)
+    else:
+        print("Error")
+print(max_3(204,14,5))
