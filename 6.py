a09c813b9daf33421c3883e51eb083c77394be2a
Galletas <a.23308051281182@cbtis128.edu.mx>
2024-11-22 23:25:55 -0600

    Create 6.py

---
+++ 6.py
@@ -0,0 +1,9 @@
+print()
+print("Edgar Gael Garcia Camacho 1182 3w:Calentamiento")
+print()
+def inversa(cadena):
+    return cadena[::-1]
+
+cadena = "estoy probando"
+resultado = inversa(cadena)
+print(resultado)  
