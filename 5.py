55c879fb93e0b3eb46e6d818b1a8757c85fbd7a9
Galletas <a.23308051281182@cbtis128.edu.mx>
2024-11-22 23:25:36 -0600

    Create 5.py

---
+++ 5.py
@@ -0,0 +1,16 @@
+print()
+print("Edgar Gael Garcia Camacho 1182 3w:Calentamiento")
+print()
+lista =[1,2,3,4]
+def suma():
+     sum= lista[0]+lista[1]+lista[2]+lista[3]
+     print(sum)
+
+def Multiplicacion():
+    mult=lista[0]*lista[1]*lista[2]*lista[3]
+    print(mult)
+
+    return
+print(suma())
+
+print(Multiplicacion())
