fcd04c8fefdad652e9dbbe8f00bb448565305f1f
Galletas <a.23308051281182@cbtis128.edu.mx>
2024-11-22 23:29:54 -0600

    Create 10.py

---
+++ 10.py
@@ -0,0 +1,10 @@
+print()
+print("Edgar Gael Garcia Camacho 1182 3w:Calentamiento")
+print()
+def procedimiento(lista):
+    for numero in lista:
+        
+        print('*' * numero)
+
+
+procedimiento([7, 19, 30])
