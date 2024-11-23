503fc7ce7637a924d2c80aece53e72be28c432e8
Galletas <a.23308051281182@cbtis128.edu.mx>
2024-11-22 23:29:30 -0600

    Create 9.py

---
+++ 9.py
@@ -0,0 +1,11 @@
+print()
+print("Edgar Gael Garcia Camacho 1182 3w:Calentamiento")
+print()
+def generar_n_caracteres(n, caracter):
+    return caracter * n
+
+print(generar_n_caracteres(5, "Pollo"))  
+print(generar_n_caracteres(3, "Sabroso"))  
+print(generar_n_caracteres(7, "*")) 
+
+
