820ebd4b6763527f7f585d62544ea6ac0e63d5d8
Galletas <a.23308051281182@cbtis128.edu.mx>
2024-11-22 23:24:56 -0600

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,15 @@
+print()
+print("Edgar gael Garcia Camacho 1182 3w:Calentamiento")
+print()
+def long(objeto):
+    contador=0
+
+    for elemento in objeto:
+
+        contador += 1
+
+    return contador
+
+listinha=[1,2,3,4,5]
+
+print(long(listinha))
